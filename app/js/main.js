d562ca077e229682096c55a6245d6baec86d7574
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-05-23 13:40:09 +0300

    add styles for the accessories section

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,10 @@
-console.log('text');
+const swiper = new Swiper('.accessories__slider', {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 40,
+
+  navigation: {
+    nextEl: '.accessories__arrow-next',
+    prevEl: '.accessories__arrow-prev',
+  },
+});
