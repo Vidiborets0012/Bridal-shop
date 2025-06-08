0af24739a686a1177fa1fbdbd9d5884b8931e0a3
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-06-08 09:58:58 +0300

    add swiperReviews

--- app/js/main.js
+++ app/js/main.js
@@ -8,3 +8,19 @@ const swiper = new Swiper('.accessories__slider', {
     prevEl: '.accessories__slider-button--prev',
   },
 });
+
+const swiperReviews = new Swiper('.reviews__slider', {
+  loop: true,
+  slidesPerView: 'auto',
+  spaceBetween: 16,
+
+  pagination: {
+    el: '.reviews__pagination',
+    type: 'fraction',
+  },
+
+  navigation: {
+    nextEl: '.reviews__arrow-next',
+    prevEl: '.reviews__arrow-prev',
+  },
+});
