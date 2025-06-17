ef071d424ffb18cd42a9f48d3d1b3d21e618a61d
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-06-17 13:20:41 +0300

    fix the content display implementation error

--- app/js/main.js
+++ app/js/main.js
@@ -25,11 +25,24 @@ const swiperReviews = new Swiper('.reviews__slider', {
   },
 });
 
-const buttons = document.querySelectorAll('.view-mode__btn');
+const viewModeButtons = document.querySelectorAll('.view-mode__btn');
+const viewModeContainer = document.querySelector('.view-mode__container');
 
-buttons.forEach(btn => {
+viewModeButtons.forEach(btn => {
   btn.addEventListener('click', () => {
-    buttons.forEach(b => b.setAttribute('aria-pressed', 'false'));
+    viewModeButtons.forEach(b => {
+      b.setAttribute('aria-pressed', 'false');
+    });
     btn.setAttribute('aria-pressed', 'true');
+
+    const mode = btn.dataset.viewMode;
+
+    viewModeContainer.classList.remove('view-mode__container--grid', 'view-mode__container--line');
+
+    if (mode === 'grid') {
+      viewModeContainer.classList.add('view-mode__container--grid');
+    } else if (mode === 'line') {
+      viewModeContainer.classList.add('view-mode__container--line');
+    }
   });
 });
